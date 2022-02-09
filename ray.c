b75e6cc3adffba8709a4ad38d9fae1e1df1668ca
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-09 13:11:11 +0100

    norminette ray.c

--- ray.c
+++ ray.c
@@ -1,16 +1,16 @@
 #include "miniRT.h"
 #include <math.h>
 
-int ray_color(t_ray r, t_sphere sphere)
+int	ray_color(t_ray r, t_sphere sphere)
 {
-	t_vec3f unit_dir;
-  
+	t_vec3f	unit_dir;
+
 	unit_dir = vec3f_unit(r.dir);
 	if (hit_sphere(sphere, r))
 	{
-		return(rgb_to_color(sphere.color));
+		return (rgb_to_color(sphere.color));
 	}
 	printf("%f ", unit_dir.x);
-	return (trgb_to_int(0, fabs(unit_dir.x) * 255, fabs(unit_dir.y) * 255, fabs(unit_dir.z) * 255));
+	return (trgb_to_int(0, fabs(unit_dir.x) * 255, fabs(unit_dir.y) * 255,
+			fabs(unit_dir.z) * 255));
 }
-
