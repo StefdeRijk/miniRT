294dc18b6c8b10fda77382aa2af08edcecdc5b63
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-02-09 13:44:58 +0100

    Removed print statement in ray.c

--- ray.c
+++ ray.c
@@ -10,7 +10,6 @@ int	ray_color(t_ray r, t_sphere sphere)
 	{
 		return (rgb_to_color(sphere.color));
 	}
-	printf("%f ", unit_dir.x);
 	return (trgb_to_int(0, fabs(unit_dir.x) * 255, fabs(unit_dir.y) * 255,
 			fabs(unit_dir.z) * 255));
 }
