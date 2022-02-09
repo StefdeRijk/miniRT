e18d35a8d7b6367da0373d4fd0da8367312c306e
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-09 13:06:32 +0100

    put hit_sphere and ray_color in header

--- ray.c
+++ ray.c
@@ -3,11 +3,13 @@
 
 int ray_color(t_ray r, t_sphere sphere)
 {
+	t_vec3f unit_dir;
+  
+	unit_dir = vec3f_unit(r.dir);
 	if (hit_sphere(sphere, r))
 	{
 		return(rgb_to_color(sphere.color));
 	}
-	t_vec3f unit_dir = vec3f_unit(r.dir);
 	printf("%f ", unit_dir.x);
 	return (trgb_to_int(0, fabs(unit_dir.x) * 255, fabs(unit_dir.y) * 255, fabs(unit_dir.z) * 255));
 }
