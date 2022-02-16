62b17421c9ef7f6aa40d5635304ab846e9de3424
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-02-16 13:27:37 +0100

    Optimalised unit_dirs

--- spot_light.c
+++ spot_light.c
@@ -29,7 +29,6 @@ t_vec3f	spot_light(t_vec3f pos, t_vec3f normal, t_scene *scene)
 	spot_unit = vec3f_sub(scene->light->pos, pos);
 	distance_sq = vec3f_len_sq(spot_unit);
 	spot_unit = vec3f_unit(spot_unit);
-	normal = vec3f_unit(normal);
 	in_product = vec3f_dot(spot_unit, normal);
 	if (in_product < 0 || in_shadow(spot_unit, pos, scene, normal))
 		return (vec3f_init(0, 0, 0));
