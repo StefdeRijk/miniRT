a3ae87a2e18361a4bfa476ad5c8ed2549bc657ee
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-16 13:00:41 +0100

    fix shadow artifacts, use different offset

--- spot_light.c
+++ spot_light.c
@@ -2,7 +2,7 @@
 
 #define SPOT_BRIGHTNESS 70
 
-int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_scene *scene)
+int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_scene *scene, t_vec3f normal)
 {
 	t_ray	to_spot;
 	t_hits	hit;
@@ -10,7 +10,7 @@ int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_scene *scene)
 	float	distance_to_spot;
 
 	to_spot.dir = spot_unit;
-	offset = vec3f_mul(to_spot.dir, 0.001);
+	offset = vec3f_mul(normal, 0.00001);
 	to_spot.origin = vec3f_add(pos, offset);
 	get_hit(&hit, scene, to_spot);
 	distance_to_spot = vec3f_len(vec3f_sub(scene->light->pos, pos));
@@ -19,10 +19,9 @@ int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_scene *scene)
 	return (0);
 }
 
-t_vec3f	spot_light(t_vec3f pos, t_vec3f dir, t_scene *scene)
+t_vec3f	spot_light(t_vec3f pos, t_vec3f normal, t_scene *scene)
 {
 	t_vec3f		spot_unit;
-	t_vec3f		ray_unit;
 	t_vec3f		spot_color;
 	float		distance_sq;
 	float		in_product;
@@ -30,9 +29,9 @@ t_vec3f	spot_light(t_vec3f pos, t_vec3f dir, t_scene *scene)
 	spot_unit = vec3f_sub(scene->light->pos, pos);
 	distance_sq = vec3f_len_sq(spot_unit);
 	spot_unit = vec3f_unit(spot_unit);
-	ray_unit = vec3f_unit(dir);
-	in_product = vec3f_dot(spot_unit, ray_unit);
-	if (in_product < 0 || in_shadow(spot_unit, pos, scene))
+	normal = vec3f_unit(normal);
+	in_product = vec3f_dot(spot_unit, normal);
+	if (in_product < 0 || in_shadow(spot_unit, pos, scene, normal))
 		return (vec3f_init(0, 0, 0));
 	spot_color = vec3f_mul(scene->light->color,
 			((scene->light->brightness * in_product) / distance_sq)
