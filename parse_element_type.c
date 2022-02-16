e8516cec537cc0519f92ae267476f824e195fa87
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-16 09:04:41 +0100

    split off parse_element_type

---
+++ parse_element_type.c
@@ -0,0 +1,39 @@
+#include "miniRT.h"
+
+void	set_element(t_parse_line *line, char c, char c2, t_scene_elem_type *t)
+{
+	if (c == 'A')
+		*t = AMBIENT;
+	else if (c == 'C')
+		*t = CAMERA;
+	else if (c == 'L')
+		*t = LIGHT;
+	else if (c == 's' && c2 == 'p')
+		*t = SPHERE;
+	else if (c == 'p' && c2 == 'l')
+		*t = PLANE;
+	else if (c == 'c' && c2 == 'y')
+		*t = CYLINDER;
+	else
+	{
+		printf("Expected element type at line %d, column %d, found '%c' (\n",
+			line->line_nr, line->i, c);
+		error("Parse error");
+	}
+}
+
+void	parse_elem_type(t_parse_line *line, t_scene_elem_type *t)
+{
+	char	c;
+	char	c2;
+
+	c2 = 0;
+	c = line->line[line->i];
+	if (c)
+		c2 = line->line[line->i + 1];
+	set_element(line, c, c2, t);
+	line->i++;
+	if (*t == SPHERE || *t == PLANE || *t == CYLINDER)
+		line->i++;
+	skip_one_or_more_char(line, ' ');
+}
--- parse_elements.c
+++ parse_elements.c
@@ -53,36 +53,3 @@ void	parse_light(t_parse_line *line, t_scene *scene)
 	parse_check_color(line, &l->color, 0, 255);
 	skip_one_or_more_char(line, '\n');
 }
-
-void	parse_elem_type(t_parse_line *line, t_scene_elem_type *t)
-{
-	char	c;
-	char	c2;
-
-	c2 = 0;
-	c = line->line[line->i];
-	if (c)
-		c2 = line->line[line->i + 1];
-	if (c == 'A')
-		*t = AMBIENT;
-	else if (c == 'C')
-		*t = CAMERA;
-	else if (c == 'L')
-		*t = LIGHT;
-	else if (c == 's' && c2 == 'p')
-		*t = SPHERE;
-	else if (c == 'p' && c2 == 'l')
-		*t = PLANE;
-	else if (c == 'c' && c2 == 'y')
-		*t = CYLINDER;
-	else
-	{
-		printf("Expected element type at line %d, column %d, found '%c' (\n",
-		line->line_nr, line->i, c);
-		error("Parse error");
-	}
-	line->i++;
-	if (*t == SPHERE || *t == PLANE || *t == CYLINDER)
-		line->i++;
-	skip_one_or_more_char(line, ' ');
-}
