856acc6bc6e8a015dac8babc13da0bcb9395076e
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-02-16 09:19:12 +0100

    Fixed some norm errors

--- sphere.c
+++ sphere.c
@@ -3,17 +3,16 @@
 
 float	hit_sphere(t_sphere sphere, t_ray r)
 {
-	t_vec3f	oc;
 	float	a;
 	float	b;
 	float	c;
 	float	solution;
-	int 	solved;
+	int		solved;
 
-	oc = vec3f_sub(r.origin, sphere.pos);
 	a = vec3f_len_sq(r.dir);
-	b = 2.0 * vec3f_dot(oc, r.dir);
-	c = vec3f_len_sq(oc) - (sphere.radius * sphere.radius);
+	b = 2.0 * vec3f_dot(vec3f_sub(r.origin, sphere.pos), r.dir);
+	c = vec3f_len_sq(vec3f_sub(r.origin, sphere.pos)) - \
+		(sphere.radius * sphere.radius);
 	solution = abc(a, b, c, &solved);
 	if (!solved)
 		return (-1.0);
