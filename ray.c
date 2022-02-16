b2daf7c3f0a8bb962626006fc391b8fdaa8111d9
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-16 10:49:33 +0100

    split off new_ray and spot_and_ambient

--- ray.c
+++ ray.c
@@ -3,11 +3,28 @@
 
 #define MAX_BOUNCES 1
 
+t_ray	new_ray(t_ray r, t_vec3f norm_dir, float hit_min)
+{
+	r.origin = at(r, hit_min);
+	r.dir = f_reflection(r.dir, norm_dir);
+	return (r);
+}
+
+t_vec3f	spot_and_ambient(t_ray r, t_vec3f object_color, t_scene *scene, t_vec3f norm_dir)
+{
+	t_vec3f				spot_color;
+	t_vec3f				ambient_color;
+
+	spot_color = spot_light(r.origin, norm_dir, scene);
+	spot_color = vec3f_mul_v(spot_color, object_color);
+	ambient_color = vec3f_mul(scene->ambient->color, scene->ambient->brightness);
+	ambient_color = vec3f_mul_v(ambient_color, object_color);
+	return (vec3f_add(spot_color, ambient_color));
+}
+
 t_vec3f	ray_color(t_ray r, t_scene *scene)
 {
 	t_vec3f				norm_dir;
-	t_vec3f				direction;
-	t_vec3f				ambient_color;
 	float				hit_min;
 	t_sphere			*spheres;
 	t_sphere			sphere;
@@ -16,11 +33,9 @@ t_vec3f	ray_color(t_ray r, t_scene *scene)
 	t_plane				plane;
 	t_cylinder			*cylinders;
 	t_cylinder			cylinder;
-	t_vec3f				spot_color;
 	t_scene_elem_type	hit_type;
 	int					hit_side_cylinder;
-	t_vec3f				color;
-
+	t_vec3f				object_color;
 
 	r.dir = vec3f_unit(r.dir);
 	spheres = scene->spheres.data;
@@ -37,13 +52,13 @@ t_vec3f	ray_color(t_ray r, t_scene *scene)
 		{
 			sphere = spheres[num];
 			norm_dir = get_normal_sphere(at(r, hit_min), sphere.pos);
-			color = sphere.color;
+			object_color = sphere.color;
 		}
 		if (hit_type == PLANE)
 		{
 			plane = planes[num];
 			norm_dir = plane_normal(plane.dir, r.dir);
-			color = plane.color;
+			object_color = plane.color;
 		}
 		if (hit_type == CYLINDER)
 		{
@@ -52,18 +67,10 @@ t_vec3f	ray_color(t_ray r, t_scene *scene)
 				norm_dir = cylinder_side_norm(at(r, hit_min), cylinder);
 			else
 				norm_dir = plane_normal(cylinder.dir, r.dir);
-			color = cylinder.color;
+			object_color = cylinder.color;
 		}
-		direction = f_reflection(r.dir, norm_dir);
-		r.origin = at(r, hit_min);
-		r.dir = direction;
-		spot_color = spot_light(r.origin, norm_dir, scene);
-		spot_color = vec3f_mul_v(spot_color, color);
-		ambient_color = vec3f_mul(scene->ambient->color, scene->ambient->brightness);
-		ambient_color = vec3f_mul_v(ambient_color, color);
-		return (vec3f_add(spot_color, ambient_color));
+		r = new_ray(r, norm_dir, hit_min);
+		return (spot_and_ambient(r, object_color, scene, norm_dir));
 	}
-	// if (scene->light && hit_light)
-	// 	return (spot_light(r, scene));
 	return (vec3f_init(0, 0, 0));
 }
