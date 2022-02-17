efac8004847f4c1849545f4c467b0d45da468af8
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-02-17 13:34:51 +0100

    Mandatory from top now looks straight down

--- cylinder_utils.c
+++ cylinder_utils.c
@@ -16,8 +16,8 @@ float	hit_top_or_bottom(t_ray ray, t_cylinder cylinder)
 	float	t_plane;
 	t_vec3f	p1;
 
-	t_plane = -(ray.origin.y - signf(ray.origin.y) * \
-		(cylinder.height / 2)) / ray.dir.y;
+	t_plane = -((ray.origin.y - signf(ray.origin.y) * \
+		(cylinder.height / 2)) / ray.dir.y);
 	p1 = at(ray, t_plane);
 	if (p1.x * p1.x + p1.z * p1.z <= cylinder.radius * cylinder.radius)
 		return (t_plane);
