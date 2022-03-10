f606706938ef752ad540055a2c50bc3abf7bca25
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-10 11:41:35 +0100

    fully norminette read_bmp

--- read_bmp.c
+++ read_bmp.c
@@ -27,53 +27,62 @@ struct	__attribute__((packed)) s_BMPInfoHeader {
 	uint32_t	colors_important;
 };
 
-int	main(void)
+void	error(char *str)
 {
-	struct s_BMPFileHeader	header;
-	struct s_BMPInfoHeader	infoheader;
-	char					dump;
-	int						fd;
-	unsigned char			*image;
-	int						i;
-	unsigned char			tmp;
-	int						skip;
+	printf("%s\n", str);
+	exit(1);
+}
 
-	fd = open("seamless-normal-map.bmp", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("error!\n");
-		exit(1);
-	}
-	read(fd, &header, sizeof(header));
-	if (header.file_type != 0x4D42)
-	{
-		printf("no bmp!\n");
-		exit(1);
-	}
-	printf("it works!\n");
-	read(fd, &infoheader, sizeof(infoheader));
-	printf("width: %d\n", infoheader.width);
-	skip = header.offset_data - sizeof(header) - sizeof(infoheader);
-	if (skip < 0)
-	{
-		printf("file corrupted!\n");
-		exit(1);
-	}
-	printf("skipped bytes: %lu\n", skip);
-	while (skip--)
-		read(fd, &dump, 1);
-	image = malloc(infoheader.size);
-	if (!image)
-		exit(1);
-	read(fd, image, infoheader.size);
-	printf("%X %X %X\n", image[0], image[1], image[2]);
+void	checked_read(int fd, void *buffer, size_t size)
+{
+	if (read(fd, buffer, size) != size)
+		error("read error");
+}
+
+void	bgr_to_rgb(unsigned char *image, uint32_t size)
+{
+	int				i;
+	unsigned char	tmp;
+
 	i = 0;
-	while (i < infoheader.size)
+	while (i < size)
 	{
 		tmp = image[i];
 		image[i] = image[i + 2];
 		image[i + 2] = tmp;
 		i += 3;
 	}
-	printf("%X %X %X\n", image[0], image[1], image[2]);
+}
+
+unsigned char	*read_bmp(char *file)
+{
+	struct s_BMPFileHeader	header;
+	struct s_BMPInfoHeader	infoheader;
+	int						fd;
+	unsigned char			*image;
+	int						skip;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		error();
+	checked_read(fd, &header, sizeof(header));
+	if (header.file_type != 0x4D42)
+		error("no bmp!");
+	checked_read(fd, &infoheader, sizeof(infoheader));
+	skip = header.offset_data - sizeof(header) - sizeof(infoheader);
+	if (skip < 0)
+		error("file corrupted!");
+	while (skip--)
+		checked_read(fd, &header, 1);
+	image = malloc(infoheader.size);
+	if (!image)
+		error("malloc failed");
+	checked_read(fd, image, infoheader.size);
+	bgr_to_rgb(image, infoheader.size);
+	return (image);
+}
+
+int	main(void)
+{
+	read_bmp("seamless-normal-map.bmp");
 }
