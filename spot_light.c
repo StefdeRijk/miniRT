472900181e93d7f367e76dca15ee3696ce675328
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-14 10:47:04 +0100

    norminette spot_light a little

--- spot_light.c
+++ spot_light.c
@@ -20,7 +20,8 @@ int	get_hit_shadow(t_light light, t_ray r, t_vec3f pos, t_scene *scene)
 	cylinder_loop_shadow(r, scene, &hit, distance_to_spot);
 	if (hit.hit_min > 0 && hit.hit_min < distance_to_spot)
 		return (1);
-	if (BONUS) {
+	if (BONUS)
+	{
 		paraboloid_loop_shadow(r, scene, &hit, distance_to_spot);
 		if (hit.hit_min > 0 && hit.hit_min < distance_to_spot)
 			return (1);
@@ -28,7 +29,8 @@ int	get_hit_shadow(t_light light, t_ray r, t_vec3f pos, t_scene *scene)
 	return (0);
 }
 
-int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_light light, t_ray r, t_scene *scene)
+int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_light light, t_ray r,
+		t_scene *scene)
 {
 	t_ray	to_spot;
 	t_vec3f	reverse_r_dir;
@@ -43,33 +45,31 @@ int	in_shadow(t_vec3f spot_unit, t_vec3f pos, t_light light, t_ray r, t_scene *s
 	return (0);
 }
 
-t_vec3f	spot_light_specular(t_vec3f normal, t_light light, t_ray new_r, t_ray old_r, t_scene *scene)
+t_vec3f	spot_light_specular(t_vec3f normal, t_light light, t_ray new_r,
+			t_ray old_r, t_scene *scene)
 {
 	t_vec3f		spot_dir;
 	t_vec3f		spot_color_specular;
 	float		in_product_specular;
 	float		alpha;
-	float		distance_sq;
-	t_vec3f		reverse_ray;
 	t_vec3f		reflected_spot;
 
 	alpha = 20;
 	spot_dir = vec3f_sub(light.pos, new_r.origin);
-	distance_sq = vec3f_len_sq(spot_dir);
-	reverse_ray = vec3f_mul(old_r.dir, 1.); //WHY not -1?
 	reflected_spot = f_reflection(spot_dir, normal);
 	reflected_spot = vec3f_unit(reflected_spot);
-	reverse_ray = vec3f_unit(reverse_ray);
-	in_product_specular = vec3f_dot(reflected_spot, reverse_ray);
-	if (in_product_specular < 0 || in_shadow(vec3f_unit(spot_dir), new_r.origin, light, old_r, scene))
+	in_product_specular = vec3f_dot(reflected_spot, vec3f_unit(old_r.dir));
+	if (in_product_specular < 0 || in_shadow(vec3f_unit(spot_dir), new_r.origin,
+			light, old_r, scene))
 		return (vec3f_init(0, 0, 0));
 	spot_color_specular = vec3f_mul(light.color, \
-		((light.brightness * powf(in_product_specular, alpha)) / distance_sq) \
-		* SPOT_BRIGHTNESS);
+		((light.brightness * powf(in_product_specular, alpha))
+				/ vec3f_len_sq(spot_dir)) * SPOT_BRIGHTNESS);
 	return (spot_color_specular);
 }
 
-t_vec3f	spot_light(t_ray new_r, t_vec3f normal, t_light light, t_ray old_r, t_scene *scene)
+t_vec3f	spot_light(t_ray new_r, t_vec3f normal, t_light light, t_ray old_r,
+		t_scene *scene)
 {
 	t_vec3f		spot_unit;
 	t_vec3f		spot_color_diffuse;
@@ -80,7 +80,8 @@ t_vec3f	spot_light(t_ray new_r, t_vec3f normal, t_light light, t_ray old_r, t_sc
 	distance_sq = vec3f_len_sq(spot_unit);
 	spot_unit = vec3f_unit(spot_unit);
 	in_product = vec3f_dot(spot_unit, normal);
-	if (in_product < 0 || in_shadow(spot_unit, new_r.origin, light, old_r, scene))
+	if (in_product < 0
+		|| in_shadow(spot_unit, new_r.origin, light, old_r, scene))
 		return (vec3f_init(0, 0, 0));
 	spot_color_diffuse = vec3f_mul(light.color, \
 		((light.brightness * in_product) / distance_sq) \
