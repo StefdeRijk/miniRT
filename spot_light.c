80a7e8a4d1b08a6de7ed5ebbc32b30f49f044467
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-15 13:38:30 +0100

    revert spotlight through mirrors

--- spot_light.c
+++ spot_light.c
@@ -69,13 +69,15 @@ t_vec3f	spot_light_specular(t_vec3f normal, t_light light, t_old_new_ray rays,
 	return (spot_color_specular);
 }
 
-t_vec3f	get_spot_color(t_vec3f spot_unit, t_old_new_ray rays, t_vec3f normal, t_light light,
+t_vec3f	spot_light(t_old_new_ray rays, t_vec3f normal, t_light light,
 		t_scene *scene)
 {
-	float	distance_sq;
-	t_vec3f	spot_color_diffuse;
-	float	in_product;
+	t_vec3f		spot_unit;
+	t_vec3f		spot_color_diffuse;
+	float		distance_sq;
+	float		in_product;
 
+	spot_unit = vec3f_sub(light.pos, rays.n.origin);
 	distance_sq = vec3f_len_sq(spot_unit);
 	spot_unit = vec3f_unit(spot_unit);
 	in_product = vec3f_dot(spot_unit, normal);
@@ -85,40 +87,5 @@ t_vec3f	get_spot_color(t_vec3f spot_unit, t_old_new_ray rays, t_vec3f normal, t_
 	spot_color_diffuse = vec3f_mul(light.color, \
 		((light.brightness * in_product) / distance_sq) \
 		* SPOT_BRIGHTNESS);
-	return (spot_color_diffuse);
-}
-
-t_vec3f	spot_light(t_old_new_ray rays, t_vec3f normal, t_light light,
-	t_scene *scene)
-{
-	t_vec3f	spot_dir;
-	t_vec3f	spot_color_diffuse;
-	t_vec3f	new_spot_pos;
-	int		i;
-	t_ray	norm_r;
-	float	dist_to_plane;
-	t_plane	plane;
-
-	i = 0;
-	spot_dir = vec3f_sub(light.pos, rays.n.origin);
-	spot_color_diffuse = get_spot_color(spot_dir, rays, normal, light, scene);
-	while (i < scene->planes.len && BONUS)
-	{
-		plane = ((t_plane *)(scene->planes.data))[i];
-		if (plane.material != MIRROR)
-		{
-			i++;
-			continue ;
-		}
-		norm_r.origin = light.pos;
-		norm_r.dir = vec3f_mul(normal, -1.);
-		dist_to_plane = hit_plane(plane.dir, plane.pos, norm_r);
-		new_spot_pos = vec3f_sub(light.pos, vec3f_mul(normal, 2 * dist_to_plane));
-		spot_dir = vec3f_sub(new_spot_pos, rays.n.origin);
-		spot_color_diffuse = vec3f_add(spot_color_diffuse, \
-			get_spot_color(spot_dir, rays, normal, light, scene));
-		i++;
-	}
-	// vec3f_print(spot_color_diffuse);
 	return (spot_color_diffuse);
 }
