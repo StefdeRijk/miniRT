304d547f4a03913059a36dd92ea4575dfdbcdbc4
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-03-16 10:35:58 +0100

    Moved functions around

--- sphere.c
+++ sphere.c
@@ -41,6 +41,25 @@ t_vec3f	get_normal_sphere(t_vec3f hit_point, t_vec3f sphere_center)
 	return (vec3f_unit(vec3f_sub(hit_point, sphere_center)));
 }
 
+float	hit_sphere(t_sphere sphere, t_ray r)
+{
+	float	a;
+	float	b;
+	float	c;
+	float	solution;
+	int		solved;
+
+	a = vec3f_len_sq(r.dir);
+	b = 2.0 * vec3f_dot(vec3f_sub(r.origin, sphere.pos), r.dir);
+	c = vec3f_len_sq(vec3f_sub(r.origin, sphere.pos)) - \
+		(sphere.radius * sphere.radius);
+	solution = abc(a, b, c, &solved);
+	if (!solved)
+		return (-1.0);
+	return (solution);
+}
+
+/*
 t_vec3f	random_in_sphere(void)
 {
 	t_vec3f	random_point;
@@ -59,21 +78,4 @@ t_vec3f	random_in_sphere(void)
 	}
 	return (vec3f_unit(random_point));
 }
-
-float	hit_sphere(t_sphere sphere, t_ray r)
-{
-	float	a;
-	float	b;
-	float	c;
-	float	solution;
-	int		solved;
-
-	a = vec3f_len_sq(r.dir);
-	b = 2.0 * vec3f_dot(vec3f_sub(r.origin, sphere.pos), r.dir);
-	c = vec3f_len_sq(vec3f_sub(r.origin, sphere.pos)) - \
-		(sphere.radius * sphere.radius);
-	solution = abc(a, b, c, &solved);
-	if (!solved)
-		return (-1.0);
-	return (solution);
-}
+*/
