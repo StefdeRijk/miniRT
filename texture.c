48e18c3a35e45722393533d68a8f2290501ddbcf
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-03-16 11:49:18 +0100

    Fixed read_bump

--- texture.c
+++ texture.c
@@ -1,4 +1,5 @@
 #include "miniRT.h"
+#include <math.h>
 
 t_vec3f read_bump(t_bmp bump_map, int x, int y, t_vec3f normal)
 {
@@ -12,6 +13,8 @@ t_vec3f read_bump(t_bmp bump_map, int x, int y, t_vec3f normal)
 	bump_normal.y = (float)bump_map.data[index + 1] / 128. - 1.;
 	bump_normal.z = -((float)bump_map.data[index + 2] / 128. - 1.);
 	angle = get_angle_to(normal, vec3f_init(0, 0, -1));
+	if (normal.z < 0)
+		angle.angle = M_PI - angle.angle;
 	bump_normal = ft_rodrigues(bump_normal, angle.k, angle.angle);
 	normal = vec3f_add(normal, bump_normal);
 	normal = vec3f_unit(normal);
