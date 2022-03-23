508416aa472a957c00588983ae7928ffd2afe541
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-23 13:56:26 +0100

    go to normal in add_bump_map

--- texture.c
+++ texture.c
@@ -6,14 +6,14 @@ t_vec3f add_bump_to_normal(t_vec3f bump_normal, t_vec3f normal)
 	t_angle	angle;
 	t_vec3f unit;
 
-	angle = get_angle_to(normal, vec3f_init(0, 0, -1));
+	angle = get_angle_to(vec3f_init(0, 0, -1), normal);
 	printf("angle: %f, k:", angle.angle);
 	vec3f_print(angle.k);
 	//if (normal.z < 0)
 	//	angle.angle = M_PI - angle.angle;
 	//printf("angle 2: %f, k:", angle.angle);
 	//vec3f_print(angle.k);
-	bump_normal = ft_rodrigues(bump_normal, angle.k, -angle.angle);
+	bump_normal = ft_rodrigues(bump_normal, angle.k, angle.angle);
 	printf("after rodrigues:");
 	vec3f_print(bump_normal);
 	normal = vec3f_add(normal, bump_normal);
